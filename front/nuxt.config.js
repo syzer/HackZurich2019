3a14df0ab7325f6b92a593d2e801cbf6daa2d1f9
djangotheman <djangotheman@users.noreply.github.com>
2019-09-28 21:40:21 +0200

    Leaflet Map

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -5,18 +5,27 @@ module.exports = {
   /*
   ** Headers of the page
   */
-  head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+ head: {
+  titleTemplate: '%s - ' + process.env.npm_package_name,
+  title: process.env.npm_package_name || '',
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+  ],
+
+  script: [
+    { src: 'https://unpkg.com/leaflet@1.5.1/dist/leaflet.js' }
+  ],
+  link: [
+    { 
+      
+      // rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+      rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.5.1/dist/leaflet.css'
+    }
+  ]
+
+},
   /*
   ** Customize the progress-bar color
   */
